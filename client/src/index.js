207a141213e4564f8fea93c77aab10b5c5d8a591
anastasiialaptii <anastasiia.laptii@gmail.com>
2025-06-08 17:28:11 -0500

    setup a client

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,22 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import React from 'react';
-import { createRoot } from 'react-dom/client'
-import GlobalStyles from './styles';
+import { createRoot } from 'react-dom/client';
 import Pages from './pages';
+import GlobalStyles from './styles';
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  uri: 'http://localhost:4000/'
+});
 
 const root = createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <GlobalStyles />
-    <Pages />
+    <ApolloProvider client={client}>
+      <GlobalStyles />
+      <Pages />
+    </ApolloProvider>
+
   </React.StrictMode>
 );
